774e56c44828989cbcf2ea400619acff81e7d9da
Mohammed Sohail <itscrazybob@gmail.com>
2020-11-11 12:02:20 +0300

    Revised lab03a

--- lab03a/src/Lab03a.java
+++ lab03a/src/Lab03a.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 /**
  * Lab03a 
  * Instructions for Lab03a are in INSTRUCTIONS.md file located in the root directory of lab03a
- * Revisions can be seen on the following GitHub URL: https://github.com/thecrazybob/CS101-lab02
+ * Revisions can be seen on the following GitHub URL: https://github.com/thecrazybob/CS101-lab03
  * Style Guidelines: http://www.cs.bilkent.edu.tr/~david/cs101/practicalwork/2010/styleguidelines.htm
- * Lab Page: http://www.cs.bilkent.edu.tr/~david/cs101/assignments/lab02/
+ * Lab Page: http://www.cs.bilkent.edu.tr/~david/cs101/assignments/lab03/
  * 
  * @author Mohammed Sohail
  * @version 11/11/2020
@@ -14,24 +14,27 @@ import java.util.Scanner;
 public class Lab03a {
     public static void main(String[] args) throws Exception {
 
-        // Initialize scanner instance
         Scanner scan = new Scanner(System.in);
         
-        int numberOfEven;
-        int numberOfOdd;
-        int n;
+        // variables
+        int number_of_even;
+        int number_of_odd;
+        int user_given_value_n;
         int sum;
+        int result_of_formula;
+
+        // program code
 
         // Initializing the variables
-        numberOfEven = 0;
-        numberOfOdd = 0;
-        n = 0;
+        number_of_even = 0;
+        number_of_odd = 0;
+        user_given_value_n = 0;
         sum = 0;
 
         // Prompt for Value "n"
         // INSTRUCTIONS: Add statements before the loop to read the integer value n from the user.
         System.out.println("Enter value");
-        n = scan.nextInt();
+        user_given_value_n = scan.nextInt();
 
         // Close the scanner
         scan.close();
@@ -47,10 +50,10 @@ public class Lab03a {
 
             // INSTRUCTIONS: count the number of even and the number of odd values, that x takes. Report the result after the loop.
             if(x % 2 == 0) {
-                numberOfEven += 1;
+                number_of_even += 1;
             }
             else {
-                numberOfOdd += 1;
+                number_of_odd += 1;
             }
 
             // INSTRUCTIONS: if x is multiple of 5 print the message "Hi Five", else if it is divisible by 2 print the message "Hi Two",
@@ -69,15 +72,15 @@ public class Lab03a {
             }
 
             // INSTRUCTIONS: sum only the values of x between 1 and n inclusive.
-            if (x <= n) {
+            if (x <= user_given_value_n) {
                 sum += x;
             }
             
         }
 
         // Print out number of even and number of odd inputs
-        System.out.println("Number of even inputs: " + numberOfEven);
-        System.out.println("Number of odd inputs: " + numberOfOdd);
+        System.out.println("Number of even inputs: " + number_of_even);
+        System.out.println("Number of odd inputs: " + number_of_odd);
         
         // Print out sum
         System.out.println("Calculated sum: " + sum);
@@ -86,11 +89,14 @@ public class Lab03a {
         // INSTRUCTIONS: with the result of the formula, n(n+1)/2, and print out a message saying whether they are the same or not!
         // INSTRUCTIONS: Test with various values of n.
 
-        int resultOfFormula = (n*(n+1))/2;
+        // Calculate result of formula
+        result_of_formula = (user_given_value_n*(user_given_value_n+1))/2;
 
-        System.out.println("Result of formula: " + resultOfFormula);
+        // Print out result of formula
+        System.out.println("Result of formula: " + result_of_formula);
 
-        if (sum == resultOfFormula) {
+        // Print out customised messaged depending on result of formula
+        if (sum == result_of_formula) {
             System.out.println("The sum of formula and sum calculated is SAME!");
         }
         else {
